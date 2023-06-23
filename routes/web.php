deeddf5601e84bf0b3eace3c3e52540a7a9dc231
putrie19 <119852247+putrie19@users.noreply.github.com>
2023-06-23 09:02:33 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/warga',[WargaController::class]);
