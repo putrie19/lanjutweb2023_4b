1b45dad04b45d65ef7dffc933a27d404877c128d
putrie19 <119852247+putrie19@users.noreply.github.com>
2023-07-02 23:54:10 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use  App\Http\Controllers\AboutController;
 use  App\Http\Controllers\WargaController;
+use  App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
