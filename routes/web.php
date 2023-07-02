000369d7fb57254969bfe24fe95199fb4b7c5df2
putrie19 <119852247+putrie19@users.noreply.github.com>
2023-07-02 23:33:19 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
     
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-    Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
 
